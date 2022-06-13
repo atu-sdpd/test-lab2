1486a124d9b90fdc25cafd3df4c171f2c6fe89c9
John French <john.french@gmit.ie>
2022-06-13 14:38:03 +0000

    Added compile error checking to test script

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,13 @@ echo "Testing $1 "
 
 testClass="$1Test"
 javac -cp ".:./test/lib/*"  ./test/$testClass.java
+if [ $? -eq 0 ]; then
+   echo Compiled successfully
+else
+   echo Failed to compile
+   exit
+fi
+
+echo  Running test...
 java -jar ./test/lib/junit-platform-console-standalone-1.9.0-M1.jar -cp ".:./test:./test/lib/system-lambda-1.2.1.jar:./test/lib/hamcrest-2.2.jar" -c $testClass
+
