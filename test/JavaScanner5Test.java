cc48830e17cdc172d6fef47407d15f530ffadb6a
John French <john.french@gmit.ie>
2022-06-13 15:27:31 +0000

    Rewrote JavaScanner5 test with regex

--- test/JavaScanner5Test.java
+++ test/JavaScanner5Test.java
@@ -2,18 +2,26 @@ import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import org.junit.jupiter.api.Test;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class JavaScanner5Test {
 
     @Test
     void testJavaScanner5() throws Exception {
-        String expected = "Enter integer number: "
-            + "Enter decimal point number: " 
-            + "First number entered was: 11\n" 
-            + "Second number entered was: 6.4\n"
-            + "Sum of numbers: 17.4\n"
-            + "Average of numbers: 8.7\n";
+        // Regular expression pattern for expected output
+        // \\W* matches one or more non-word characters (i.e. not [a-zA-Z_0-9])
+        // (useful for variations in spacing, use of colons etc)
+        String expectedOutput = "Enter integer number\\W*" 
+            + "Enter decimal point number\\W*"
+            + "First number entered was\\W*11\\n"
+            + "Second number entered was\\W*6.4\\n"
+            + "Sum of numbers\\W*17.4\\n*" 
+            + "Average of numbers\\W*8.7\\n*";
+
+        // Compiling the pattern with the CASE_INSENSITIVE flag will make the 
+        // resulting matches case insensitive
+        Pattern expectedPattern = Pattern.compile(expectedOutput, Pattern.CASE_INSENSITIVE);
        
         String output = tapSystemOut(() -> {
             withTextFromSystemIn("11", "6.4")
@@ -21,6 +29,6 @@ public class JavaScanner5Test {
                     JavaScanner5.main(new String[0]);
                 });           
         });
-        assertThat(expected, equalTo(output));
+        assertThat(output, matchesPattern(expectedPattern));
     }
 }
